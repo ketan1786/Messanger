65f9ab8935bf66918b591cf385e7c18743fef7b7
ketan pathak <ketanpathak@outlook.com>
2022-02-10 14:20:51 +0530

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StreamChat } from 'stream-chat';
+import { OverlayProvider, Chat, ChannelList, Channel } from "stream-chat-expo"
+import { Text } from 'react-native';
+
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+
+const API_KEY = "pratvm5p2vjm"
+const client = StreamChat.getInstance(API_KEY)
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
+  const [isReady, setIsReady] = useState(false)
+  const [selectedChannel, setSelectedChannel] = useState<any> (null)
+
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: 'ketan',
+          name: 'ketan pathak',
+          image: 'https://i.imgur.com/fR9Jz14.png',
+        },
+        client.devToken('ketan'),
+      )
+
+      // Create a Channel 
+      const channel = client.channel("messaging", "Public", { 
+        name: 'Public Chat Room' 
+      })
+      await channel.watch()
+
+      setIsReady(true)
+     
+    }
+
+    connectUser()
+
+    return () => client.disconnectUser()
+  }, [])
+  
+  const onChannelPressed = (channel) => {
+    setSelectedChannel(channel)
+  }
+
+  if (!isLoadingComplete || !isReady) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <OverlayProvider>
+        <Chat client={client}>
+
+        {selectedChannel ? (
+          <Channel channel={selectedChannel}>
+          <Text
+           style={{ marginTop: 50 }}
+           onPress={() => setSelectedChannel(null)}>
+             Go Back
+          </Text>
+          </Channel>
+        ) : (
+          <ChannelList onSelect={onChannelPressed} />
+        )}
+
+        
+        {/* <Navigation colorScheme={colorScheme} /> */}
+        </Chat>
+        </OverlayProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
